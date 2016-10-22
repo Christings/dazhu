f2d1c32dbf89e9853ddc42dfb43651aa4bab8bf2
yinzihao <yinzihao1@126.com>
2016-10-22 11:59:44 +0800

    add func:search can include blog body 	modified:   dazhu/templates/search/result.html 	modified:   search/views.py

--- search/views.py
+++ search/views.py
@@ -18,8 +18,55 @@ class result(TemplateView):
         keywords = urllib.unquote(keywords)
         tools.debug('search key2', keywords)
 
-        posts = BlogPost.objects.filter(title__contains=keywords)
+        posts_title = BlogPost.objects.filter(title__contains=keywords)
+        posts_body = BlogPost.objects.filter(body__contains=keywords)
+
+        final_result_map = {}
+        final_result = []
+
+        class SearchItem(object):
+            pass
+
+        for item in posts_title:
+            if item.guid not in final_result_map:
+                final_result_map[item.guid] = True
+                tempItem = SearchItem()
+                tempItem.guid = item.guid
+                tempItem.category = item.category
+                tempItem.title = self.GetContent(item.title,keywords)
+                tempItem.search_content = self.GetContent(item.body,keywords)
+                final_result.append(tempItem)
+
+        for item in posts_body:
+            if item.guid not in final_result_map:
+                final_result_map[item.guid] = True
+                tempItem = SearchItem()
+                tempItem.guid = item.guid
+                tempItem.category = item.category
+                tempItem.title = self.GetContent(item.title,keywords)
+                tempItem.search_content = self.GetContent(item.body,keywords)
+                final_result.append(tempItem)
+
 
         context['title'] = keywords
-        context['posts'] = posts
+        context['posts'] = final_result
         return context
+
+    def GetContent(self,input_str,keyword):
+        format_str="<span style='color:red;'>@#k</span>"
+        content_length = 60
+        tools.debug("GetContent ",input_str)
+
+        input_str = tools.RemoveHttpStr(input_str)
+        fmtkeyword = format_str.replace("@#k",keyword)
+        input_str = input_str.replace(keyword,fmtkeyword)
+        index= input_str.find(keyword)
+
+        start = index - content_length
+        if start < 0:
+            start = 0
+        end = index + content_length
+        if end > len(input_str):
+            end = len(input_str)
+
+        return input_str[start:end]
