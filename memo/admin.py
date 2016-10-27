8d468061bfe260a1d12d1c81925a83169c9a1943
yinzihao <yinzihao1@126.com>
2016-10-27 22:50:54 +0800

    change memo admin : show title author instead of show body. modified:   memo/admin.py

--- memo/admin.py
+++ memo/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 import models
 # Register your models here.
 class Admin(admin.ModelAdmin):
-    list_display = ('body', )
+    list_display = ('title', 'author',)
 
 admin.site.register(models.Memo,Admin)
