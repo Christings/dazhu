ee6c981396e46ce77602be8073099a22712cd2ae
yinzihao <yinziao1@126.com>
2017-03-07 22:26:01 +0800

    change album rolate

--- album/views.py
+++ album/views.py
@@ -27,7 +27,7 @@ def getAlbum(pages,is_login):
     pageCount = 24
     tools.debug("islogin",is_login)
     pages = int(pages)
-    
+
     allCounts = Photoes.objects.count()
     allPages = allCounts // pageCount + 1
     posts = None
@@ -35,11 +35,11 @@ def getAlbum(pages,is_login):
         posts = Photoes.objects.all().order_by("-timestamp")[(pages - 1) * pageCount:pages * pageCount]
     else:
         posts = Photoes.objects.filter(phototype='public').order_by("-timestamp")[(pages - 1) * pageCount:pages * pageCount]
-    
+
     return posts,allPages
 
-class index(TemplateView):    
-    template_name = "album/index.html" 
+class index(TemplateView):
+    template_name = "album/index.html"
     def get_context_data(self, **kwargs):
         context = super(index, self).get_context_data(**kwargs)
         pages = 1
@@ -47,16 +47,16 @@ class index(TemplateView):
             pages = self.args[0]
         except:
             pass
-        pages = int(pages)        
+        pages = int(pages)
         posts = getAlbum(pages,self.request.user.is_authenticated())[0]
-        
+
         context['posts'] = posts
         context['title'] = u'相册'
-        
+
         return context
-    
+
 class show(TemplateView):
-    template_name = "album/show.html" 
+    template_name = "album/show.html"
     def get_context_data(self, **kwargs):
         context = super(show, self).get_context_data(**kwargs)
         inputName = None
@@ -65,7 +65,7 @@ class show(TemplateView):
         except:
             pass
         pic = Photoes.objects.get(rndName = inputName)
-        
+
         context['pic'] = pic
         context['title'] = pic.showName
         next = Photoes()
@@ -88,32 +88,32 @@ def ajaxGetAlbum(request,pages):
         pages=int(pages)
     except:
         pages = 1
-    
+
     pagesInfo = getAlbum(pages,request.user.is_authenticated())
     posts = pagesInfo[0]
     allpages = pagesInfo[1]
-    
+
     if allpages < pages:
         tools.debug("allpages < pages ",allpages,pages)
-        return HttpResponse()    
-    
+        return HttpResponse()
+
     result = []
     for item in posts:
         result.append({'rndName':item.rndName,'showName':item.showName,})
-    
+
     return HttpResponse(json.dumps(result, ensure_ascii=False))
 
 def ajaxGetFirst6Album(request):
     pages = 1
-    
+
     pagesInfo = getAlbum(pages,request.user.is_authenticated())
     posts = pagesInfo[0]
     allpages = pagesInfo[1]
-    
+
     if allpages < pages:
         tools.debug("allpages < pages ",allpages,pages)
-        return HttpResponse()    
-    
+        return HttpResponse()
+
     result = []
     i = 0
     for item in posts:
@@ -121,32 +121,32 @@ def ajaxGetFirst6Album(request):
         if i==7:
             break
         result.append({'rndName':item.rndName,'showName':item.showName,})
-    
+
     return HttpResponse(json.dumps(result, ensure_ascii=False))
 
 
 @csrf_exempt
 @secu.login_filter
 def upload(request):
-    """ 上传文件 """  
-    source_filename = request.POST.get('name', '')    
-    tools.debug(source_filename)  
+    """ 上传文件 """
+    source_filename = request.POST.get('name', '')
+    tools.debug(source_filename)
     # print("request aid is ",request.POST['aid'])
     fileObj = request.FILES.get('file')
-    tools.debug(fileObj)  
-    
-    response = HttpResponse()  
-    
-    if fileObj: 
+    tools.debug(fileObj)
+
+    response = HttpResponse()
+
+    if fileObj:
         filename = fileObj.name.encode("utf8")
         fileExt = ""
         fileInfo = []
-        if "." in filename:            
+        if "." in filename:
             fileInfo = filename.split('.')
             fileExt = fileInfo[len(fileInfo) - 1]
             fileExt = '.' + fileExt
             tools.debug("fileext is " + fileExt)
-            
+
         def name_add_rndnum(input_name):
             tools.debug("name_add_rndnum", input_name)
             file_name = os.path.split(input_name)
@@ -157,19 +157,19 @@ def upload(request):
 
         file_name = name_add_rndnum(filename)[0]
         tools.debug("文件名",file_name)
-        
+
         album_folder = dazhu.settings.BASE_DIR + "/dazhu/static/album/"
         if not os.path.exists(album_folder):
             os.makedirs(album_folder)
-            
+
         mini_folder = album_folder + "mini/"
         if not os.path.exists(mini_folder):
             os.makedirs(mini_folder)
-            
+
         normal_folder = album_folder + "normal/"
         if not os.path.exists(normal_folder):
             os.makedirs(normal_folder)
-        
+
         fileData = tools.readFile(dazhu.settings.BASE_DIR + "/dazhu/static/ueditor/net/config.json")
         allowExt = None
         configJson = json.loads(fileData)
@@ -179,22 +179,22 @@ def upload(request):
             tools.debug("json load file types error")
             tools.debug(errors)
             return response
-        
+
         if allowExt == None:
             tools.debug("不能获取 file types")
             return response
-    
-        
+
+
         if fileExt.lower() in allowExt:
             # file_path = normal_folder + file_name + "tmp" + fileExt
             mini_path = mini_folder + file_name + fileExt
             normal_path = normal_folder + file_name + fileExt
-            
+
             try:
                 with open(normal_path, 'wb+') as f:
                     for chunk in fileObj.chunks():
                         f.write(chunk)
-                
+
                 #make mini pic
                 make_thumbnail(normal_path, mini_path)
 
@@ -223,20 +223,20 @@ def upload(request):
 
 def rolate_pic(request, inputName):
     pic = Photoes.objects.get(rndName = inputName)
-    
+
     normal_path = u"".join([settings.BASE_DIR,"/dazhu/static/album/normal/", pic.rndName]).encode("utf-8")
     im = Image.open(normal_path)
-    im.rotate(90, expand=True).save(normal_path)
+    im.rotate(-90, expand=True).save(normal_path)
 
     mini_path = u"".join([settings.BASE_DIR,"/dazhu/static/album/mini/", pic.rndName]).encode("utf-8")
     make_thumbnail(normal_path, mini_path)
-    
+
     redict_path = u"".join(["/album/show/", inputName])
     tools.debug("rolate_pic", redict_path)
     return redirect(redict_path)
 
 def make_thumbnail(normal_path, mini_path):
-    # make small pic            
+    # make small pic
     im = Image.open(normal_path)
     im.thumbnail((180, 180))
 
@@ -258,39 +258,39 @@ def make_thumbnail(normal_path, mini_path):
 
     im.save(mini_path)
 
-#等比例压缩图片  
-def resizeImg(img, output_path, dst_w=0, dst_h=0, qua=85):  
-    ''''' 
-    只给了宽或者高，或者两个都给了，然后取比例合适的 
-    如果图片比给要压缩的尺寸都要小，就不压缩了 
-    '''  
-    ori_w, ori_h = img.size  
-    widthRatio = heightRatio = None  
-    ratio = 1  
-  
-    if (ori_w and ori_w > dst_w) or (ori_h and ori_h  > dst_h):  
-        if dst_w and ori_w > dst_w:  
-            widthRatio = float(dst_w) / ori_w                                      #正确获取小数的方式  
-        if dst_h and ori_h > dst_h:  
-            heightRatio = float(dst_h) / ori_h  
-  
-        if widthRatio and heightRatio:  
-            if widthRatio < heightRatio:  
-                ratio = widthRatio  
-            else:  
-                ratio = heightRatio  
-  
-        if widthRatio and not heightRatio:  
-            ratio = widthRatio  
-  
-        if heightRatio and not widthRatio:  
-            ratio = heightRatio  
-  
-        newWidth = int(ori_w * ratio)  
-        newHeight = int(ori_h * ratio)  
-    else:  
-        newWidth = ori_w  
-        newHeight = ori_h  
+#等比例压缩图片
+def resizeImg(img, output_path, dst_w=0, dst_h=0, qua=85):
+    '''''
+    只给了宽或者高，或者两个都给了，然后取比例合适的
+    如果图片比给要压缩的尺寸都要小，就不压缩了
+    '''
+    ori_w, ori_h = img.size
+    widthRatio = heightRatio = None
+    ratio = 1
+
+    if (ori_w and ori_w > dst_w) or (ori_h and ori_h  > dst_h):
+        if dst_w and ori_w > dst_w:
+            widthRatio = float(dst_w) / ori_w                                      #正确获取小数的方式
+        if dst_h and ori_h > dst_h:
+            heightRatio = float(dst_h) / ori_h
+
+        if widthRatio and heightRatio:
+            if widthRatio < heightRatio:
+                ratio = widthRatio
+            else:
+                ratio = heightRatio
+
+        if widthRatio and not heightRatio:
+            ratio = widthRatio
+
+        if heightRatio and not widthRatio:
+            ratio = heightRatio
+
+        newWidth = int(ori_w * ratio)
+        newHeight = int(ori_h * ratio)
+    else:
+        newWidth = ori_w
+        newHeight = ori_h
     tools.debug("resizeImg width %s height %s" % (newWidth, newHeight))
     img.resize((newWidth,newHeight),Image.ANTIALIAS).save(output_path, quality=qua)
 
@@ -323,7 +323,3 @@ def recover(request):
     #     item.phototype = "private"
     #     item.save()
     return HttpResponse()
-
-
-
-
